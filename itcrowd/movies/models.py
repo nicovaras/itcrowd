d6155f607afede6c840f90e1576af6897f2c6fe0
Nico Varaschin <nicovaras22@gmail.com>
2020-04-11 11:31:32 -0300

    Show aliases in Person

--- itcrowd/movies/models.py
+++ itcrowd/movies/models.py
@@ -9,7 +9,7 @@ class Person(models.Model):
 
 class Alias(models.Model):
     alias = models.CharField(max_length=100, blank=False)
-    person = models.ForeignKey(Person,on_delete=models.CASCADE)
+    person = models.ForeignKey(Person,on_delete=models.CASCADE, related_name='aliases')
 
     def __str__(self):
         return "{} '{}' {}".format(self.person.first_name, self.alias, self.person.last_name)
--- itcrowd/movies/serializers.py
+++ itcrowd/movies/serializers.py
@@ -13,18 +13,19 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         model = Group
         fields = ['url', 'name']
 
-
 class AliasSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = Alias
-		fields = ['alias', 'person']
+    class Meta:
+        model = Alias
+        fields = ['alias', 'person']
+
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Person
+        fields = ['first_name', 'last_name', 'movies_as_actor', 'movies_as_director', 'movies_as_producer', 'aliases']
+
 
 class MovieSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Movie
         fields = ['title', 'release_year', 'actors', 'directors', 'producers']
 
-class PersonSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Person
-        fields = ['first_name', 'last_name']
